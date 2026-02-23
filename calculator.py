b8e7770fb1278377398d860dd038c181493b0dda
Teena-sheeba <vtu30599@veltech.edu.in>
2026-02-23 19:06:18 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,32 @@
+while True:
+    print("\nSimple Calculator")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print("Choose operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    if choice == '1':
+        print("Result:", num1 + num2)
+    elif choice == '2':
+        print("Result:", num1 - num2)
+    elif choice == '3':
+        print("Result:", num1 * num2)
+    elif choice == '4':
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Cannot divide by zero")
+    else:
+        print("Invalid input")
+
+    again = input("Do you want to calculate again? (yes/no): ")
+    if again.lower() != "yes":
+        print("Thank you for using calculator!")
+        break
